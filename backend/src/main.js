d3296b437eb7f2af25db28a7bb1477295e5bd1a4
ahmaddzidnii <zdngaming312@gmail.com>
2023-11-30 16:35:48 +0700

    settings environment backend

--- backend/src/main.js
+++ backend/src/main.js
@@ -1,10 +1,13 @@
 import dotenv from "dotenv";
 import express from "express";
 import cors from "cors";
+import router from "./routes/public-route.js";
 dotenv.config();
 
 const app = express();
 app.use(cors());
+app.use(express.json());
+app.use(router);
 
 const port = process.env.PORT || 2000;
 app.listen(port, () => {
