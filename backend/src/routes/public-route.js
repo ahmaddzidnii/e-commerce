8e1d16a23c463e797ba825185d69a2a8c72bd08a
ahmaddzidnii <zdngaming312@gmail.com>
2023-12-17 12:57:52 +0700

    backend: add: add dummy data

--- backend/src/routes/public-route.js
+++ backend/src/routes/public-route.js
@@ -1,6 +1,7 @@
 import express from "express";
 import userController from "../controller/user.controller.js";
 import { refreshTokenController } from "../controller/refresh-token-controler.js";
+import { db } from "../../lib/db.js";
 
 const publicRouter = express.Router();
 publicRouter.post("/register", userController.register);
@@ -12,4 +13,11 @@ publicRouter.get("/auth/google", userController.loginWithGoogle);
 // Google Callback Login
 publicRouter.get("/auth/google/callback", userController.callbackLoginGoogle);
 
+publicRouter.get("/products", async (req, res) => {
+  const products = await db.product.findMany();
+  return res.json({
+    message: "Hello World",
+    data: products,
+  });
+});
 export { publicRouter };
