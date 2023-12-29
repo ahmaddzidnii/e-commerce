8d6b782f203887bbbfcd83ee4f46f4d628dfbf2b
ahmaddzidnii <zdngaming312@gmail.com>
2023-12-29 13:09:19 +0700

    backend: feature: add api search product

--- backend/src/service/search-product-service.js
+++ backend/src/service/search-product-service.js
@@ -36,6 +36,85 @@ const searchHintService = async (req) => {
   return search_hint;
 };
 
+const searchProductService = async (req) => {
+  let { query, limit, page } = req.query;
+
+  if (!query) {
+    throw new ResponseError("search not found", 404);
+  }
+  if (!limit || limit < 48 || isNaN(limit)) {
+    limit = 48;
+  }
+
+  if (!page || page < 1 || isNaN(page)) {
+    page = 1;
+  }
+
+  const itemsPerPage = parseInt(limit);
+  const currentPage = parseInt(page);
+
+  const countProduct = await db.product.count({
+    where: {
+      OR: [
+        {
+          product_name: {
+            search: query,
+          },
+        },
+        {
+          product_name: {
+            contains: query,
+          },
+        },
+      ],
+    },
+  });
+
+  const totalPages = Math.ceil(countProduct / itemsPerPage);
+  const offset = (currentPage - 1) * itemsPerPage;
+
+  // Menghitung jumlah item yang harus ditampilkan dalam halaman terakhir
+  const itemsInLastPage = countProduct % itemsPerPage;
+  const count = currentPage === totalPages ? itemsInLastPage : itemsPerPage;
+
+  const products = await db.product.findMany({
+    where: {
+      OR: [
+        {
+          product_name: {
+            search: query,
+          },
+        },
+        {
+          product_name: {
+            contains: query,
+          },
+        },
+      ],
+    },
+    take: itemsPerPage,
+    skip: offset,
+  });
+
+  // Kondisi dimana tidak memiliki halaman selanjutnya
+  const hasNextPage = totalPages === currentPage ? false : true;
+
+  // Kondisi dimana tidak memiliki halaman sebelumnya
+  const hasPrevPage = currentPage === 1 ? false : true;
+
+  const data = {
+    products,
+    pagination: {
+      current_page: currentPage,
+      total_page: totalPages,
+      count: count,
+      has_next_page: hasNextPage,
+      has_prev_page: hasPrevPage,
+    },
+  };
+  return data;
+};
 export default {
   searchHintService,
+  searchProductService,
 };
